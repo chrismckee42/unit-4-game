77a3c335b31412f578517c4c0dae19a6b5c8124a
Christopher McKee <chris.mckee.13@gmail.com>
2019-10-20 19:46:37 -0700

    adds working but static character image portraits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,16 +14,16 @@ characters[1] = new Character("Anakin Skywalker", "Anakin.jpg", 100, 1, 1)
 characters[2] = new Character("Obi-Wan Kenobi", "Obi-Wan.jpg", 100, 1, 1)
 characters[3] = new Character("Qui-Gon Jinn", "Qui-Gon-Jinn.jpg", 100, 1, 1)
 
-for (let i = 0; i < characters.length; i++) {
-    const newChar = $("<span>");
-    newChar.addClass("char")
-    newChar.append('<img src="assets/images/Jar-Jar-Binks.jpg" alt="Jar-Jar">')
-    newChar.append('<div>Jar-Jar</div>')
-    newChar.append('<span>HP: 100</span>')
-    newChar.append('<span>Attack: 1</span>')
-    newChar.append('<span>Counter Attack: 1</span>')
-    $(".characters").append(newChar)
-}
+// for (let i = 0; i < characters.length; i++) {
+//     const newChar = $("<span>");
+//     newChar.addClass("char")
+//     newChar.append('<img src="assets/images/Jar-Jar-Binks.jpg" alt="Jar-Jar">')
+//     newChar.append('<div>Jar-Jar</div>')
+//     newChar.append('<span>HP: 100</span>')
+//     newChar.append('<span>Attack: 1</span>')
+//     newChar.append('<span>Counter Attack: 1</span>')
+//     $(".characters").append(newChar)
+// }
 
 $(document).ready(function () {
 
